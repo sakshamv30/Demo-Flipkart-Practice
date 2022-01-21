63d20adbad14c3d3ceadf5b94c7f2b445598c3b7
Saksham <sakshamv30@gmail.com>
2022-01-21 14:44:15 +0530

    update

--- src/com/flipkart/application/CustomerApplication.java
+++ src/com/flipkart/application/CustomerApplication.java
@@ -3,6 +3,8 @@
  */
 package com.flipkart.application;
 
+import java.util.Scanner;
+
 import com.flipkart.business.CustomerService;
 
 /**
@@ -18,10 +20,43 @@ public class CustomerApplication {
 		// TODO Auto-generated method stub
 		//create the instance of customer service
 		CustomerService service = new CustomerService();
-		service.createCustomer();
-		service.updateCustomer();
-		service.deleteCustomer();
-		service.listCustomer();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("1 - Create Customer");
+		System.out.println("2 - Update Customer");
+		System.out.println("3 - List Customer");
+		System.out.println("4- Delete Customer");
+		System.out.println("5 - QUIT");
+		
+		boolean quit=false;
+		do {
+
+			
+			int a=scanner.nextInt();
+		switch(a) {
+		case 1:service.createCustomer();
+		break;
+		
+		case 2:service.updateCustomer();
+		break;
+		
+		
+		case 3:service.listCustomer();
+		break;
+		
+		
+		case 4:service.deleteCustomer();
+		break;
+		
+		case 5:quit=true;
+		break;
+		
+		default: System.out.println("Error Input");
+		break;
+		
+		} 
+		
+		}while(!quit);
+		
 	}
 
 }
